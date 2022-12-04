7e44a2586bf78a609e110b588c5fff3ceeaf7d82
Quentin Schuller <schullerquentin@gmail.com>
2022-12-04 19:48:31 +0100

    aoc(2022): day04 ok

--- Scripts/init.ps1
+++ Scripts/init.ps1
@@ -2,17 +2,31 @@ param ($year, $day)
 
 if (!$year)
 {
-    $year = Get-Date -UFormat "%yyyy"
+    $year = Get-Date -format "yyyy"
 }
 
 if (!$day)
 {
-    $day = Get-Date -UFormat "%d"
+    $day = Get-Date -format "dd"
+    $dayUrl = $day.TrimStart('0')
 }
+else
+{
+    if ($day.Length == 1)
+    {
+        $dayUrl = $day
+        $day = '0' + $day
+    }
+    else
+    {
+        $dayUrl = $day.TrimStart('0')
+    }
+}
+
 
 $currentPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 $repo = Split-Path $currentPath -Parent
-$url = "https://adventofcode.com/"+$year+"/day/"+$day
-powershell $currentPath\fetch-data.ps1 -year $year -day $day -repo $repo
-powershell $currentPath\new.ps1 -year $year -day $day -repo $repo
+$url = "https://adventofcode.com/"+$year+"/day/"+$dayUrl
+& $currentPath\fetch-data.ps1 -year $year -day $day -repo $repo
+& $currentPath\new.ps1 -year $year -day $day -repo $repo
 Start-Process $url
